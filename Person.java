6b01ad5e0084ef07570609404bfc58cbad9abf1c
shivam <shivampoddartech@gmail.com>
2025-03-05 01:18:41 +0530

    exaple JavaLibrary

---
+++ Person.java
@@ -0,0 +1,9 @@
+package JavaLibrary;
+
+public class Person {
+	public static void main(String[] args) {
+		Person p = new Person();
+		System.out.println(p);
+		System.out.println(p.toString());
+	}
+}
